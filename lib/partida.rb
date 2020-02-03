6b336c298b11e7a5c920fab884e2194177f0ebc1
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-02-03 18:41:51 -0300

    Implement some game rules inside the match

--- lib/partida.rb
+++ lib/partida.rb
@@ -3,8 +3,6 @@ class Partida
     @jogadas = params[:jogadas]
   end
 
-  private
-
   def jogador_casa
     @jogadas.first.jogador_casa
   end
@@ -14,19 +12,33 @@ class Partida
   end
 
   def total_pedras_casa
-    @jogadas.inject(0) do |pedras, jogada|
+    total_pedras = @jogadas.inject(0) do |pedras, jogada|
       pedras + jogada.resultado_casa.num_pedras
     end
+    jogador_casa.pontuacao_final += total_pedras
   end
 
   def total_pedras_fora
-    @jogadas.inject(0) do |pedras, jogada|
+    total_pedras = @jogadas.inject(0) do |pedras, jogada|
       pedras + jogada.resultado_fora.num_pedras
     end
+    jogador_fora.pontuacao_final += total_pedras
   end
 
   def jogador_vitorioso
     return jogador_casa if total_pedras_casa > total_pedras_fora
     return jogador_fora if total_pedras_fora > total_pedras_casa
+  end
+
+  private
+
+  def checa_pedras
+    pedras_casa = @jogadas.map { |jogada| jogada.resultado_casa.num_pedras }
+    pedras_fora = @jogadas.map { |jogada| jogada.resultado_fora.num_pedras }
+    adiciona_bonus(resultado_casa) if pedras_casa.uniq.size <= 1
+    adiciona_bonus(resultado_fora) if pedras_fora.uniq.size <= 1
+  end
+
+  def adiciona_bonus(resultado)
   end
 end
